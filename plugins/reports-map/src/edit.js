52b33b6e757270694b53210f1487a6649f8cc20d
Alex Colby <alexanderdcolby@gmail.com>
2023-07-09 22:14:15 +0100

    rewrite: factored out CSV sources to a component (probably should have made some commits before now...)

--- plugins/reports-map/src/edit.js
+++ plugins/reports-map/src/edit.js
@@ -20,23 +20,62 @@ import { useBlockProps } from "@wordpress/block-editor";
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
 import "./editor.scss";
+
 import { TextControl, TabPanel, FormFileUpload } from "@wordpress/components";
-import { useState, useMemo } from "@wordpress/element";
+import { useState } from "@wordpress/element";
 
 /**
- * A text input that validates the input as a url to a CSV file
+ * Converts a File object to a data url asynchronously
+ *
+ * @param {File} file the file to read
+ * @returns {Promise<string>} the data url for the file
+ */
+function read_to_url(file) {
+	return new Promise((resolve) => {
+		const reader = new FileReader();
+		reader.onloadend = () => resolve(reader.result);
+		reader.readAsDataURL(file);
+	});
+}
+
+/**
+ * A file input that converts the input to a data url
  *
  * @param {any} param0 The parameters for the CSVInput component
- * @param {(url: string) => void} param0.onChange A callback that is called when the input is changed to a valid CSV url
+ * @param {(url: string, csv: any[]) => void} param0.onChange A callback that is called when a file is uploaded
  * @returns {WPElement}
  */
-function CSVUrlSource({ onChange }) {
+const CsvFileSource = ({ onChange }) => (
+	<FormFileUpload
+		accept="text/csv"
+		variant="secondary"
+		className="file-input"
+		onChange={async ({ target: { files } }) => {
+			console.log(files[0]);
+			const url = await read_to_url(files[0]);
+			console.log(url);
+			onChange(url);
+		}}
+	>
+		{__("Upload .csv File", "reports-map")}
+	</FormFileUpload>
+);
+
+/**
+ * A text input that validates the input as a url to the given content type
+ *
+ * @param {any} param0 The parameters for the CSVInput component
+ * @param {(url: string, csv: any[]) => void} param0.onChange A callback that is provided with the url and the parsed csv
+ * @returns {WPElement}
+ */
+const CsvUrlSource = ({ onChange }) => {
 	const [valid, setValid] = useState(false);
 
 	return (
 		<TextControl
 			type="url"
-			className={valid ? "csv-input" : "csv-input invalid-input"}
+			label={__("Remote Url", "reports-map")}
+			className="csv-input"
 			onChange={async (url) => {
 				try {
 					const response = await fetch(url);
@@ -49,15 +88,32 @@ function CSVUrlSource({ onChange }) {
 			}}
 		/>
 	);
-}
+};
 
-function read_to_url(file) {
-	return new Promise((resolve) => {
-		const reader = new FileReader();
-		reader.onloadend = () => resolve(reader.result);
-		reader.readAsDataURL(file);
-	});
-}
+/**
+ * An input that allows the user to load a csv from a file or external url
+ *
+ * @param {any} param0 The parameters for the CSVInput component
+ * @param {(url: string, csv: any[]) => void} param0.onChange A callback that is provided with the url and the parsed csv
+ * @returns {WPElement}
+ */
+const CsvSource = ({ onChange }) => (
+	<TabPanel
+		tabs={[
+			{
+				name: "file",
+				title: __("File Upload", "reports-map"),
+				content: <CsvFileSource onChange={onChange} />,
+			},
+			{
+				name: "url",
+				title: __("External URL", "reports-map"),
+				content: <CsvUrlSource onChange={onChange} />,
+			},
+		]}
+		children={(tab) => tab.content}
+	/>
+);
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -67,39 +123,10 @@ function read_to_url(file) {
  *
  * @return {WPElement} Element to render.
  */
-export default function Edit({ attributes, setAttributes }) {
-	const file_input = (
-		<FormFileUpload
-			accept="text/csv"
-			onChange={async (ev) => {
-				console.log(ev.target.files[0]);
-				const url = await read_to_url(ev.target.files[0]);
-				console.log(url);
-			}}
-		>
-			{__("Upload .csv File", "reports-map")}
-		</FormFileUpload>
-	);
-
-	const url_input = <CSVInput onChange={(url) => setAttributes({ url })} />;
-
+export default function Edit({ setAttributes }) {
 	return (
 		<div {...useBlockProps()}>
-			<TabPanel
-				tabs={[
-					{
-						name: "file",
-						title: __("File Upload", "reports-map"),
-						content: file_input,
-					},
-					{
-						name: "url",
-						title: __("External URL", "reports-map"),
-						content: url_input,
-					},
-				]}
-				children={(tab) => tab.content}
-			/>
+			<CsvSource onChange={(url) => setAttributes({ url })} />
 		</div>
 	);
 }
