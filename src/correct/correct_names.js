bd020967aee1032d536ac5a32325dd9d7243424e
Alex Colby <alexanderdcolby@gmail.com>
2023-07-11 10:42:18 +0100

    feat: fetches and cleans coroner names - also fetches title and role of coroners

---
+++ src/correct/correct_names.js
@@ -0,0 +1,112 @@
+import { fetch_html, map_async, map_series } from '../fetch/helpers.js'
+
+/**
+ * Fetches the list of page urls from the coroner society website
+ *
+ * @param {string} url the coroner society url
+ * @returns {Promise<string[]>} the list of page urls
+ */
+async function fetch_page_urls(url) {
+  const $ = await fetch_html(url)
+
+  // determine how many pages we need to search and the urls for those pages
+  const last_path = 'ul.paging > li.last > a'
+  const last_url = $(last_path).attr('href')
+  const last_num = Number(last_url.match(/\/coroners\/_\/(\d+)\//)[1])
+
+  return Array.from({ length: last_num }, (_, i) => `${url}_/${i + 1}/`)
+}
+
+/**
+ * Fetches a non-empty html document from a url
+ * We sometimes receive an empty document from the coroner society website
+ *
+ * @param {string} url the url to fetch
+ * @param {number} [retries=10] the number of times to retry fetching the url
+ * @returns {Promise<CheerioAPI>} the parsed html document
+ */
+async function fetch_non_empty(url, retries = 10) {
+  let $
+  for (let i = 0; i < retries; i++) {
+    $ = await fetch_html(url)
+    if ($.length > 2) return $
+  }
+  console.log('empty document', url)
+  return $
+}
+
+/**
+ * Fetches the list of coroners from a coroner page
+ *
+ * @param {string} page_url the url of the coroner page to fetch names from
+ * @returns {Promise<{name: string, title: string, role: string, email: string}[]>} the list of coroners on the page
+ */
+async function fetch_page(page_url) {
+  const $ = await fetch_html(page_url)
+  const people = $('ul.people > li')
+  if (people.length === 0) console.log(`"${$.html()}"`)
+
+  return people.get().map(person => {
+    // fetches the name, title, role, and email of a coroner
+    // role is a bit dodgy here, it can be an area, "Retired", "Honorary", etc
+    // email may not always be present, but the rest seem consistent
+    const elem = $(person)
+    const name = elem.find('a').text()
+    const [title, role] = elem
+      .find('span')
+      .get()
+      .map(span => $(span).text())
+
+    return { name, title, role }
+  })
+}
+
+/**
+ * Fetches the list of coroners from the coroner society website
+ *
+ * @param {string} url the coroner society url
+ * @returns {Promise<{name: string, title: string, role: string, email: string}[]>} the list of coroners
+ */
+async function fetch_name_list(url) {
+  const page_urls = await fetch_page_urls(url)
+  console.log(page_urls)
+  const pages = await map_series(
+    page_urls,
+    fetch_page,
+    'Fetching coroners |:bar| :current/:total pages'
+  )
+  return pages.flat()
+}
+
+/**
+ * Removes all titles from a name
+ *
+ * @param {string} name the name to strip
+ * @returns {string} the name with titles removed
+ */
+function remove_titles(name) {
+  const titles =
+    /(Mr|Mrs|Ms|Dr|Miss|Judge|Cdr|Justice|The|Hon|HHJ|QC)(\s+\.?|$)/gi
+  return name.replace(titles, '')
+}
+
+/**
+ * Removes the [email protected] text from a name
+ *
+ * @param {string} name the name to strip
+ * @returns {string} the name with email block removed
+ */
+function remove_email_block(name) {
+  return name.replace(/\[.*\]/g, '')
+}
+
+const coroners = await fetch_name_list(
+  'https://www.coronersociety.org.uk/coroners/'
+)
+const coroner_map = Object.fromEntries(
+  coroners.map(({ name, ...coroner }) => {
+    name = remove_email_block(name)
+    return [remove_titles(name), { name, ...coroner }]
+  })
+)
+console.log(coroner_map)
