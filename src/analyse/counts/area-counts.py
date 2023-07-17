8a601cccc33c99131e094d944b93243858a25984
Alex Colby <alexanderdcolby@gmail.com>
2023-07-17 19:20:32 +0200

    feat: prints statistics for area counts

--- src/analyse/counts/area-counts.py
+++ src/analyse/counts/area-counts.py
@@ -30,8 +30,19 @@ reports['year'] = reports['date_of_report'].str.extract(r'\d{2}\/\d{2}\/(\d{4})'
 # count the number of reports in each year
 grouped_counts = reports.groupby(['year', 'coroner_area']).size().reset_index(name='count')
 area_counts = grouped_counts.pivot(index='year', columns='coroner_area', values='count').fillna(0).astype(int)
+
+# %% [markdown]
+# ### Various statistics about the counts
+
 sum_counts = pd.DataFrame(area_counts.sum()).rename(columns={0: 'count'})
 
+print(f"Total number of reports: {sum_counts.sum().sum()}")
+print(f"Number of coroner areas: {len(sum_counts.columns)}")
+print(f"Mean number of reports: {sum_counts.mean().mean()}")
+print(f"Median number of reports: {sum_counts.median().median()}")
+print(f"IQR of number of reports: {sum_counts.mean().quantile([0.25, 0.75])}")
+
+print(f"Sorted counts: {sum_counts.sort_values(by='count', ascending=False)}")
 
 # %% [markdown]
 # ### Saving the results
