cee1a4b0eb27b209222188dd349bbf697cefa810
Alex Colby <alexanderdcolby@gmail.com>
2023-07-18 10:58:13 +0200

    fix: updates summed counts to the new format - this is better for making bar graphs

--- src/analyse/counts/category-counts.py
+++ src/analyse/counts/category-counts.py
@@ -41,11 +41,13 @@ for category in categories:
   reports[category] = reports['category'].str.contains(category, regex=False)
 
 category_counts = reports[categories].groupby(reports['year']).sum()
+sum_counts = pd.DataFrame(category_counts.sum()).rename(columns={0: 'count'})
+sum_counts = sum_counts.sort_values(by='count', ascending=False)
+sum_counts.index.name = 'category'
 category_counts
 
 # %% [markdown]
 # ### Saving the results
 
 category_counts.to_csv(f"{DATA_PATH}/category-years.csv")
-sum_counts = pd.DataFrame(category_counts.sum()).transpose()
-sum_counts.to_csv(f"{DATA_PATH}/category-counts.csv", index=False)
+sum_counts.to_csv(f"{DATA_PATH}/category-counts.csv")
