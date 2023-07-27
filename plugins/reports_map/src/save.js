cddffd3cb24709563e37f3da385cf026ccf8f87d
Alex Colby <alexanderdcolby@gmail.com>
2023-07-27 11:10:19 +0100

    feat: adds fetching support on the frontend

--- plugins/reports_map/src/save.js
+++ plugins/reports_map/src/save.js
@@ -9,11 +9,41 @@ import { useBlockProps } from "@wordpress/block-editor";
 import Papa from "papaparse";
 import ReportHeatMap from "./heatmap/ReportHeatmap.js";
 import color_scales from "./heatmap/report-scales.json";
+import { useEffect, useState } from "@wordpress/element";
+import { useMemo } from "react";
 
-export function SaveBlock({ csv_text }) {
+/** @typedef {{csv_text: string, source_url: string}} SaveBlockProps */
+
+/** Maps a function over the values of an object, leaving keys unaffected */
+const object_map = (obj, fn) =>
+	Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, fn(v)]));
+
+/**
+ * Renders the saved content of the block.
+ * We split this out to allow it to be rendered both in the editor, on the
+ * server, and on the frontend.
+ * @param {SaveBlockProps} props
+ */
+export function SaveBlock({ csv_text, source_url }) {
 	// TODO: replace this with:
 	// 1. CSVs loaded on the frontend (for freshness)
 	// 2. Time selection controls
+	const [csv_, setCsv] = useState([]);
+
+	// this might be okay depending on how react tests if a returned value is a
+	// cleanup function, otherwise it might try to call a promise as a function
+	useEffect(async () => {
+		const url = source_url;
+		const response = await fetch(url);
+		const text = await response.text();
+		let { data } = Papa.parse(text, { header: true, skipEmptyLines: true });
+		const csv = data.map((row) => object_map(row, parseInt));
+		// if we're still on the same url, update the csv
+		if (url === source_url) setCsv(csv);
+	}, [source_url]);
+
+	const filtered = useMemo(() => csv_.filter({ year }));
+
 	const csv = Papa.parse(csv_text, { header: true, skipEmptyLines: true }).data;
 	if (csv.length === 0) return <div>No data</div>;
 	const area_counts = Object.fromEntries(
@@ -26,7 +56,7 @@ export function SaveBlock({ csv_text }) {
 			max={max}
 			scale={color_scales.custom}
 			className="report-heatmap-block"
-			data-props={JSON.stringify({ csv_text })}
+			data-props={JSON.stringify({ csv_text, source_url })}
 		/>
 	);
 }
@@ -40,10 +70,10 @@ export function SaveBlock({ csv_text }) {
  *
  * @return {WPElement} Element to render.
  */
-export default function Save({ attributes: { csv_text } }) {
+export default function Save({ attributes }) {
 	return (
 		<div {...useBlockProps.save()}>
-			<SaveBlock csv_text={csv_text} />
+			<SaveBlock {...attributes} />
 		</div>
 	);
 }
