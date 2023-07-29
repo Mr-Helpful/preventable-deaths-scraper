c63c9291b68ecdc87caabcf3c03d5759c1f89b68
Alex Colby <alexanderdcolby@gmail.com>
2023-07-29 19:33:57 +0100

    feat: adds argument parsing for adding corrections

--- src/correct/add_corrections.js
+++ src/correct/add_corrections.js
@@ -1,3 +1,18 @@
-import { program } from 'commander'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import fs from 'fs/promises'
 
-program.description('Add manual corrections to pre-existing corrections')
+import { program, Argument } from 'commander'
+
+const path = dirname(fileURLToPath(import.meta.url))
+const names = await fs.readdir(`${path}/failed_parses`)
+const fields = names.map(name => name.split('.')[0])
+console.log(fields)
+const field_arg = new Argument('field', 'Field to add corrections to').choices(
+  fields
+)
+
+const { field } = program
+  .description('Add manual corrections to pre-existing corrections')
+  .addArgument(field_arg.argRequired())
+  .parse()
