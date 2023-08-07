ad380966bc47c8d0615632a85a887a5a4b76fc0f
Alex Colby <alexanderdcolby@gmail.com>
2023-08-07 14:58:06 +0100

    feat: deduplicates similar organisation names

--- src/correct/simplify_destination.js
+++ src/correct/simplify_destination.js
@@ -6,6 +6,14 @@ export const conjunctions = /and|or|of|the|for/gi
 export const conjunctive_words = re`\b${conjunctions}\b`
 
 /**
+ * @param {string} organisation the organisation to check
+ * @returns {boolean} whether the organisation has an acronym
+ */
+export function has_acronym(organisation) {
+  return /\b[A-Z]{2,}\b/.test(organisation)
+}
+
+/**
  * Returns the acronym of an organisation name
  * @param {string} organisation the organisation to convert
  * @returns {string} the acronym of the organisation
@@ -17,8 +25,7 @@ export function to_acronym(organisation) {
     .filter(word => !conjunctions.test(word))
   // if there's less than 3 words or there's an acronym
   // we probably shouldn't compress it further
-  if (words.length < 3 || words.some(word => word === word.toUpperCase()))
-    return organisation
+  if (words.length < 3 || has_acronym(organisation)) return organisation
 
   return words.map(word => word[0]).join('')
 }
