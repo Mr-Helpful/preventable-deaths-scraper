7fb153454c5498ebdaff502dd29f1ad3358f0640
Alex Colby <alexanderdcolby@gmail.com>
2023-08-11 11:49:38 +0100

    feat: updates correct_destinations for hierachies - honestly there's a lot in here, just me bashing my head against a wall   until something drops out, so I'm not covering everything here.

--- src/correct/correct_destinations.js
+++ src/correct/correct_destinations.js
@@ -3,6 +3,7 @@ import {
   heirichic_matches,
   hierachic_match,
   max_by,
+  non_words,
   try_matches
 } from './approx_match.js'
 import {
@@ -12,7 +13,7 @@ import {
   has_acronym,
   to_acronym
 } from './simplify_destination.js'
-import { merge_failed, load_correction_data } from './helpers.js'
+import { merge_failed, load_correction_data, re } from './helpers.js'
 
 /* Assumptions
 
@@ -31,33 +32,38 @@ we'll be relying on is that:
   can consider the whole field an unmatched name
 */
 
+export const punctuation = re`${conjunctive_words}|[^\w'-]+`
+
+const object_map = (obj, key_func = x => x, value_func = x => x) =>
+  Object.fromEntries(
+    Object.entries(obj).map(([k, v]) => [key_func(k), value_func(v)])
+  )
+
+/**
+ * Removes all matched segments from the text
+ * @param {string} text the text to remove from
+ * @param {{loc: [number, number]}[]} matches the matches to remove
+ * @returns {string} text with the matches removed
+ */
+export function remove_matches(text, matches) {
+  const [str, start] = matches.reduce(
+    ([str, start], { loc: [s, e] }) => [str + text.slice(start, s), e],
+    ['', 0]
+  )
+  return str + text.slice(start)
+}
+
 /**
  * Tests whether a text only comprises of a list of names, up to connectives
  * and punctuation
  * @param {string} text the text to test the names against
- * @param {string[]} names the names to test
+ * @param {{loc: [number, number]}[]} matches the names to test
  * @returns {boolean} whether the text only contains the names
  */
-function only_contains(text, names) {
-  const names_words = new Set(names.flatMap(name => name.split(/[^\w]+/g)))
-  const text_words = text
-    .split(/[^\w]+/g)
-    .filter(word => !conjunctions.test(word))
-  return text_words.every(word => names_words.has(word))
-}
-
-/**
- * Attempts to match a list of names against a text, and only returns a match
- * if the text only contains the matched names (i.e. no other possible
- * organisation names are left unmatched)
- * @param {string} text the text to match names within
- * @param {string[]} names the organisation names to match against
- * @returns {string[] | undefined} the matches, or undefined if no good match
- */
-function try_complete_matching(text, names) {
-  const known_key_map = Object.fromEntries(names.map(name => [name, name]))
-  const known_matches = try_matches(text, known_key_map, 2, 0.2)
-  if (known_matches && only_contains(text, known_matches)) return known_matches
+export function is_complete_match(text, matches) {
+  const non_match = remove_matches(text, matches)
+  const without_punctuation = non_match.replace(punctuation, '')
+  return without_punctuation.length === 0
 }
 
 /**
@@ -72,35 +78,38 @@ export default async function Corrector(keep_failed = true) {
   )
   if (!keep_failed) failed = []
 
-  for (const correction of corrections) {
-    for (const [key, value] of Object.entries(correction)) {
-      const simple = key
-        .replace(conjunctive_words, '')
-        .replace(/[^\w']+/g, ' ')
-        .trim()
-      delete correction[key]
-      correction[simple] = value
-    }
-  }
   if (corrections.length < 2) corrections.unshift({}, {})
-  let known_replacements = corrections.slice(0, 2)
+  let known_replacements = [{}, {}]
+  for (const key of Object.keys(corrections[0])) {
+    if (!try_known_match(key)) add_to_known(key)
+  }
+  corrections[0] = known_replacements[0]
+  corrections[1] = known_replacements[1]
 
   function try_known_match(text) {
     if (known_replacements[1][text]) return known_replacements[1][text]
 
-    const simple = text.replace(conjunctive_words, '')
-    const known_keys = Object.keys(known_replacements[0])
-    const known_matches = hierachic_match(simple, known_keys)
+    let replacements = Object.assign({}, ...known_replacements)
+    replacements = object_map(replacements, text =>
+      text.replace(punctuation, '').trim()
+    )
+    const known_keys = Object.keys(replacements)
+    const known_matches = hierachic_match(
+      text.replace(punctuation, '').trim(),
+      known_keys
+    )
     if (known_matches === undefined) return undefined
 
     const known_match = max_by(known_matches, match => -match.error).phrase
-    return known_replacements[0][known_match]
+    return replacements[known_match]
   }
 
   function add_to_known(text) {
-    const simple = text.replace(conjunctive_words, '')
-    known_replacements[0][simple] = text
-    if (!has_acronym(text)) known_replacements[1][to_acronym(simple)] = text
+    text = text.replace(non_words, ' ').trim()
+    delete known_replacements[0][to_acronym(text)]
+    known_replacements[0][text] = text
+    if (to_acronym(text) !== text)
+      known_replacements[1][to_acronym(text)] = text
   }
 
   /** @param {string} text */
@@ -110,15 +119,18 @@ export default async function Corrector(keep_failed = true) {
 
     // if we have `;` or `|` in the text we can assume it's a well formed list
     if (text.match(/[;|]/)) {
-      let destinations = text.split(/[;|]/g).map(dest => dest.trim())
-      destinations = destinations.flatMap(dest => {
-        if (dest.length === 0) return ''
-        const known_match = try_known_match(dest)
-        if (known_match) return known_match
-        add_to_known(dest)
-        return dest
-      })
-      return destinations.filter(dest => dest.length > 0).join(' | ')
+      const destinations = text
+        .split(/[;|]/g)
+        .map(dest => dest.trim())
+        .filter(dest => dest.length > 0)
+      return destinations
+        .map(dest => {
+          const known_match = try_known_match(dest)
+          if (known_match) return known_match
+          add_to_known(dest)
+          return dest
+        })
+        .join(' | ')
     }
 
     // if there's no connectives or punctuation, we can just return the text
@@ -129,17 +141,13 @@ export default async function Corrector(keep_failed = true) {
       return text
     }
 
-    // remove conjunctions i.e. `and`, `or` as they get in the way of matching
-    // const simple = text.replace(conjunctive_words, '')
-    // const all_keys = corrections.flatMap(correction => Object.keys(correction))
-    // let matches = heirichic_matches(simple, all_keys)
-    // if (matches && only_contains(text, matches)) {
-    //   const full_map = Object.assign({}, ...corrections)
-    //   return matches
-    //     .filter(match => match.length > 0)
-    //     .map(match => full_map[match])
-    //     .join('|')
-    // }
+    // if the text can be built from known matches and connectives, with only a
+    // few errors, we can return those matches
+    const replacements = Object.assign({}, ...corrections)
+    const matches = heirichic_matches(text, Object.keys(replacements))
+    if (matches && is_complete_match(text, matches))
+      return matches.map(match => replacements[match.phrase]).join(' | ')
+
     failed.push(text)
   }
 
