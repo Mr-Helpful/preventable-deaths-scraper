474c4f36db359bc7ab1edf1d22832b026a550d40
Alex Colby <alexanderdcolby@gmail.com>
2023-08-21 17:48:35 +0100

    feat: adds progress bar for report merging

--- src/correct/correct_destinations.js
+++ src/correct/correct_destinations.js
@@ -15,6 +15,7 @@ import {
   to_acronym
 } from './simplify_destination.js'
 import { merge_failed, load_correction_data, re } from './helpers.js'
+import { map_series } from '../fetch/helpers.js'
 
 /* Assumptions
 
@@ -76,9 +77,11 @@ export default async function Corrector(keep_failed = true) {
 
   if (corrections.length < 2) corrections.unshift({}, {})
   let known_replacements = [{}, {}]
-  for (const key of Object.keys(corrections[0])) {
-    if (!try_known_match(key)) add_to_known(key)
-  }
+  await map_series(
+    Object.keys(corrections[0]),
+    key => !try_known_match(key) && add_to_known(key),
+    'Merging destinations |:bar| :current/:total corrections'
+  )
   corrections[0] = known_replacements[0]
   corrections[1] = known_replacements[1]
 
