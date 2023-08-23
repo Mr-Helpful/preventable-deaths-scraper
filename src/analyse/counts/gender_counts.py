0fadd483905acaf6950e173737b97f05dc438759
Alex Colby <alexanderdcolby@gmail.com>
2023-08-23 19:36:54 +0100

    tweak: splits out coroner society stats for gender

--- src/analyse/counts/gender_counts.py
+++ src/analyse/counts/gender_counts.py
@@ -60,13 +60,16 @@ sum_counts = reports.value_counts('gender')
 # %% [markdown]
 # ### Various statistics about the counts
 
-toml_stats['coroner gender'] = statistics = {
-  "no. coroners from society male": website_counts['male'],
-  "no. coroners from society female": website_counts['female'],
-  "no. coroners from society unknown": website_counts['unknown'],
+toml_stats['coroner genders in reports'] = statistics = {
   "no. reports from male coroners": sum_counts['male'],
   "no. reports from female coroners": sum_counts['female'],
   "no. reports from unknown coroners": sum_counts['unknown'],
+}
+
+toml_stats["coroner's society genders"] = {
+  "no. coroners from society male": website_counts['male'],
+  "no. coroners from society female": website_counts['female'],
+  "no. coroners from society unknown": website_counts['unknown'],
 }
 
 print(f"Gender count statistics: {statistics}")
